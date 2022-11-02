04f49018f9b5586eb8a70b338bd7a199058a4794
Vishal Puneyani <vishal.puneyani21@gmail.com>
2022-11-02 13:58:57 +0530

    Queue Concepts

--- DSAL/Week7/QueueUsingStack.c
+++ DSAL/Week7/QueueUsingStack.c
@@ -5,12 +5,17 @@ typedef struct
     int arr[10];
     int top,size;
 } Stack;
-
+int isEmpty(Stack* s)
+{
+    if(s->top==-1)
+        return 1;
+    else
+        return 0;
+}
 void push(Stack *s,int x)
 {
     s->arr[++s->top] = x;
 }
-
 int pop(Stack *s)
 {
     return s->arr[s->top--];
@@ -18,8 +23,8 @@ int pop(Stack *s)
 void display(Stack* s)
 {
     int i;
-    printf("Queue is : ");
-    for(i=0;i<s->top;i++)
+    printf("Queue is : \n");
+    for(i=0;i<=s->top;i++)
         printf("%d\n",s->arr[i]);
 }
 int main()
@@ -33,14 +38,15 @@ int main()
     printf("Enter max queue size : ");
     scanf("%d",&n);
 
-    s1.size=s2.size=n;
-    printf("\n1. Enqueue\t2. Dequeue\t3.Display\t0. Exit\n");
+    s1.size=n;
+    s2.size=n;
+    printf("\n1.Enqueue\t2.Dequeue\t3.Display\t0.Exit\n");
     do
     {
-        printf("Enter choice : ");
+        printf("\nEnter choice : ");
         scanf("%d",&choice);
 
-        if (choice == 1)
+        if(choice==1)
         {
             printf("Enter number : ");
             scanf("%d",&item);
@@ -51,13 +57,15 @@ int main()
             if (s2.top == -1)
             {
                 while (s1.top != -1)
-                {
                     push(&s2,pop(&s1));
-                }
             }
-            printf("Number dequeued: %d",pop(&s2));
         }
         else if(choice==3)
-            display(&s2);
-    } while (choice != 0);
+        {
+            if(isEmpty(&s1))
+                display(&s2);
+            if(isEmpty(&s2))
+                display(&s1);
+        }
+    }while (choice != 0);
 }
